8033b76b0c4eca60506094c56ce4036f5ab5198f
Camilo <camilo.coo250@gmail.com>
2019-04-26 20:55:52 +0000

    finished migrating to gatsby

---
+++ src/components/footer.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import Store from "../store/appContext.jsx";
+
+
+export class Footer extends Component {
+	render() {
+		return (
+			<div>
+				<footer className="footer gradientFooter">
+					<div className="container-fluid pt-5">
+						<div className="row d-flex justify-content-around">
+							<div className="col-4 text-left">Contact Us</div>
+							<div className="col-4 text-center">Donations</div>
+							<div className="col-4 text-right">
+								<a>Spanish</a>
+								<br />
+								<a>English</a>
+							</div>
+						</div>
+						<div className="row text-center">
+							<div className="col-12">
+								Copyright ©{" "}
+								<script type="text/javascript">document.write(new Date().getFullYear());</script>. All
+								right reserved
+							</div>
+						</div>
+					</div>
+				</footer>
+			</div>
+		);
+	}
+}
+
+export default Store(Footer)
